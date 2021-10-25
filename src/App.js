d2ddd2ef757a0cff1e4a97f5075803ab8e949b14
forrestburton <burton.forrest10@gmail.com>
2021-10-25 14:44:52 -0700

    Can read users.txt to display number of users

--- src/App.js
+++ src/App.js
@@ -1,36 +1,46 @@
-import { Layout, Menu, Breadcrumb } from 'antd';
+import { Layout } from 'antd';
 import RAM from './graphs/ram.png';
 import CPU from './graphs/cpu.png';
 import LUCID_LOGO from './graphs/logo.png';
-import { useState, useEffect } from 'react';
+import NUM_USERS from './graphs/users.txt';
+import { useEffect } from 'react';
 const { Header, Content, Footer } = Layout;
-//var shell = require('shelljs');
-
 
 
 function App() {
-  const [numUsers, setNumUsers] = useState(0);
-
-  
   
   useEffect(async() => {      
-    
+    // Read contents of users.txt, which contains the number of users on the bender 
+    var rawFile = new XMLHttpRequest();
+    rawFile.open("GET", NUM_USERS, false);
+    rawFile.onreadystatechange = function ()
+    {
+        if(rawFile.readyState === 4)
+        {
+            if(rawFile.status === 200 || rawFile.status == 0)
+            {
+                var allText = rawFile.responseText;
+                console.log(allText);
+                document.getElementById('output').textContent=allText;  // Set number of users in HTML
+                
+            }
+        }
+    }
+    rawFile.send(null);
   });
-
 
   return (
     <Layout className="layout">
     <Header>
-      {/* <h1 style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', fontSize: "40px"}}>Dashboard</h1> */}
       <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center'}}>
-        <img src={LUCID_LOGO}  /> 
+        <img src={LUCID_LOGO} /> 
       </div>
     </Header>
     <br/>
     <br/>
     <br/>
     <Content style={{ padding: '0 50px' }}>
-      <span style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', fontSize: "20px"}}><b style={{ fontSize: "35px"}}> Bender 0&nbsp;</b> **4 Users**</span>
+      <span style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', fontSize: "20px"}}><b style={{ fontSize: "35px"}}> Bender 0&nbsp;</b> **<span id="output"></span> &nbsp;User(s)**</span>
       <hr/>
       <div className="site-layout-content">
         <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center'}}>
